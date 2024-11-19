e0622e56dc69b0a36b761458f3dc2ed4b5025fcb
User <User@DESKTOP-8SJ542C>
2024-11-20 01:47:25 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "Header.h"
+
+int main() {
+    int sum = add(3, 4);
+    int product = multiply(3, 4);
+
+    std::cout << "Sum: " << sum << std::endl;
+    std::cout << "Product: " << product << std::endl;
+
+    return 0;
+}
